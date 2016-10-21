3abd8d6feada4d329c5c516c747079ea5f7d6e31
Johnny Barton <johnnybarton411@gmail.com>
2016-10-21 00:34:47 -0400

    Pin connections for RPi and nRF

--- Code/AlarmClock.py
+++ Code/AlarmClock.py
@@ -1,5 +1,18 @@
+# Connecting the Raspberry Pi to the NRF24L01+
+
+# NRF24L01+			Raspberry Pi
+#   VCC --------------------------- 3.3V	(Pin 01)
+#   GND --------------------------- GND		(Pin 06)
+#   CSN --------------------------- GPIO	(Pin 24)
+#   CE  --------------------------- GPIO17	(Pin 11)
+#  MOSI --------------------------- GPIO10	(Pin 19)
+#  MISO --------------------------- GPIO9	(Pin 21)
+#  SCK ---------------------------- GPIO11	(Pin CLK)
+#   IRQ - NOT CONNECTED
+
 import time
-impot os
+import os
+import sys
 
 DEBUG = 1
 
@@ -12,14 +25,24 @@ if lsa != 2:
 	print "Usage: AlarmClock.py <time>"
 	sys.exit(1)
 
-time = sa[1]
-
+wake_up_time = sa[1]
 try:
-	hour = int(time[:2])
-	minutes = int(time[2:4])
+	hour = int(wake_up_time[:2])
+	minute = int(wake_up_time[2:4])
 
+	if hour < 0 or hour > 23:
+		raise ValueError('Hour must be between 0-23')
+	if minute < 0 or minute > 59:
+		raise ValueError('Minute must be between 0-59')
+except ValueError as e:
+	print e.args
+
+while True:
+	now = time.localtime()
+	if hour == now.tm_hour and minute == now.tm_min:
+		print "WAKE TF UP!!!"
+		break
 	
+	time.sleep(61 - now.tm_sec)
 
-except ValueError:
-	
-
+print "Exited successfully"
